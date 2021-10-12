c9bf912bc48fc92eb70f5e07b9f7118aaee94349
shubha <shubha07658@gmail.com>
2021-10-13 00:19:54 +0530

    arithmetic

---
+++ arithmetic.js
@@ -0,0 +1,29 @@
+var num1 = 11
+var num2 = 22
+var num3 = 32
+
+// addition
+//console.log(num1 + num2 + num3)
+
+//substraction
+//console.log(num3 - num2 - num1)
+
+//multiply
+//console.log(num1 *  num2 * num3)
+
+// division
+//console.log(num2/num1)
+
+//modules
+//console.log(num2 % num1)
+
+console.log(num1++)
+console.log(num1++) //post
+
+console.log(++num1) //pre
+
+// decrement - post and pre
+console.log(num1--)
+console.log(num1--)
+
+console.log(--num1)
